36c76c26eb14f05e8270678740336b29d2b901fb
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-07-06 03:47:23 +0900

    added fwupdmgr and update the lock

--- system/services.nix
+++ system/services.nix
@@ -2,6 +2,7 @@
 {
   services.xserver.enable = true;
   services.flatpak.enable = true;
+  services.fwupd.enable = true;
 
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
