5d23e0a04d34a65ec353b2a0e982f5911e94ef16
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-07-21 23:03:45 +0100

    removed home-manager out

--- home/gnome_apps.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, lib, ... }:
-{
-
-  home.activation.setGnomeDarkMode = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    ${pkgs.glib}/bin/gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-    ${pkgs.glib}/bin/gsettings set org.gnome.desktop.interface clock-format '24h'
-    ${pkgs.glib}/bin/gsettings set org.gnome.desktop.interface clock-show-weekday true
-  '';
-}
