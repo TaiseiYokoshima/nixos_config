540eb0ff4b8cb209fccc771ce53ca51e9155c8ca
TaiseiYokoshima <taiseiyokoshima030712@gmail.com>
2025-12-22 16:01:16 +0000

    removed home-manager from inputs

--- flake.nix
+++ flake.nix
@@ -5,53 +5,47 @@
       nixpkgs.url = "nixpkgs/nixos-25.11";
    };
 
+   outputs = { nixpkgs, ... }:
+      let
+         lib = nixpkgs.lib;
+         system = "x86_64-linux";
+         pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
 
-   outputs = { self, nixpkgs, home-manager, ... }: 
-   let 
-      lib = nixpkgs.lib;
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-   in 
-   {
+         nixosConfigurations = {
 
-      nixosConfigurations = {
-
-         dell_laptop = lib.nixosSystem {
-            inherit system;
-            modules = [ 
-               ./configuration.nix 
-            ];
-            specialArgs = {
-               hardwarePath = ./hardware/dell_laptop/afb403a99e904bc996fd65ebfb69c9f6.nix;
+            dell_laptop = lib.nixosSystem {
+               inherit system;
+               modules = [
+                  ./configuration.nix
+               ];
+               specialArgs = {
+                  hardwarePath = ./hardware/dell_laptop/afb403a99e904bc996fd65ebfb69c9f6.nix;
+               };
             };
-         };
 
-
-
-         intel = lib.nixosSystem {
-            inherit system;
-            modules = [ 
-               ./configuration.nix 
-            ];
-            specialArgs = {
-               cpu = "intel";
+            intel = lib.nixosSystem {
+               inherit system;
+               modules = [
+                  ./configuration.nix
+               ];
+               specialArgs = {
+                  cpu = "intel";
+               };
             };
-         };
 
-         amd = lib.nixosSystem {
-            inherit system;
-            modules = [ 
-               ./configuration.nix 
-            ];
-            specialArgs = {
-               cpu = "amd";
+            amd = lib.nixosSystem {
+               inherit system;
+               modules = [
+                  ./configuration.nix
+               ];
+               specialArgs = {
+                  cpu = "amd";
+               };
             };
+
          };
-
-
 
       };
-
-
-   };
 }
