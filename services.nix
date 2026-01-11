f8549989b8783c9c0ff767e847d07036f8c305e4
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2026-01-11 23:05:43 +0000

    adjusted to the new convetion for gnone and gdm config

--- services.nix
+++ services.nix
@@ -1,11 +1,10 @@
-{ config, ... }:
+{ ... }:
 {
    services.flatpak.enable = true;
    services.fwupd.enable = true;
 
-   services.xserver.displayManager.gdm.enable = true;
-   services.xserver.desktopManager.gnome.enable = true;
-
+   services.displayManager.gdm.enable = true;
+   services.desktopManager.gnome.enable = true;
 
    services.pulseaudio.enable = false;
    security.rtkit.enable = true;
