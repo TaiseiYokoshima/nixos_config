d12fc48815a8ae215afbca09acc27d638b15c18b
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2026-08-30 19:15:15 +0100

    added kvm and qemu

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -1,10 +1,22 @@
-{...}: {
-    virtualisation = {
+{ pkgs, ... }: {
+   boot.kernelModules = [
+      "kvm-intel"
+      "kvm-amd"
+   ];
+
+   virtualisation = {
       docker.enable = true;
 
       libvirtd = {
-        enable = true;
-        # qemu.networks.default.forwardMode = "bridge";
+         enable = true;
+         qemu = {
+            package = pkgs.qemu_kvm;
+            runAsRoot = true;
+            swtpm.enable = true; # Habilita TPM virtual (necesario para Windows 11)
+         };
+         # qemu.networks.default.forwardMode = "bridge";
       };
-    };
+   };
+
+   users.users.rom.extraGroups = [ "libvirtd" ];
 }
