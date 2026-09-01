7dc3082ea7e96fb14bec38bfab5ae3cf6269fbaa
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2026-09-02 00:13:15 +0100

    codex fixed the live usb iso

--- hosts/live/live.nix
+++ hosts/live/live.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, modulesPath, lib, ... }:
+{ config, inputs, pkgs, modulesPath, lib, ... }:
 let
    modules = inputs.self.modules;
    hardware = inputs.self.hardware;
@@ -6,29 +6,79 @@ in
 {
    system.stateVersion = "26.05";
 
+   users.users.nixos.enable = lib.mkForce false;
+   users.users.rom = {
+      initialHashedPassword = lib.mkForce "";
+      extraGroups = lib.mkForce [
+         "networkmanager"
+         "wheel"
+         "docker"
+         "kvm"
+         "libvirtd"
+         "video"
+      ];
+   };
+   users.users.root.initialHashedPassword = lib.mkForce "";
 
+   services.getty.autologinUser = lib.mkForce "rom";
+   services.displayManager.autoLogin.enable = lib.mkForce false;
+   services.getty.helpLine = lib.mkForce (
+      ''
+         The "rom" and "root" accounts have empty passwords.
+
+         To log in over ssh you must set a password for either "rom" or "root"
+         with `passwd` (prefix with `sudo` for "root"), or add your public key to
+         /home/rom/.ssh/authorized_keys or /root/.ssh/authorized_keys.
+
+         To set up a wireless connection, run `nmtui`.
+      ''
+      + lib.optionalString config.services.xserver.enable ''
+
+         Type `sudo systemctl start display-manager' to
+         start the graphical user interface.
+      ''
+   );
+
+   nix.settings.trusted-users = lib.mkForce [ "rom" ];
+
+   boot.postBootCommands = lib.mkAfter ''
+      for o in $(</proc/cmdline); do
+         case "$o" in
+            live.rom.passwd=*)
+               set -- $(IFS==; echo $o)
+               echo "rom:$2" | ${pkgs.shadow}/bin/chpasswd
+               ;;
+         esac
+      done
+   '';
 
    systemd.services.populate-user-config = {
       description = "Populate live user configuration";
 
       wantedBy = [ "multi-user.target" ];
       after = [ "systemd-user-sessions.service" ];
+      path = with pkgs; [
+         bash
+         coreutils
+         util-linux
+      ];
 
       serviceConfig = {
          Type = "oneshot";
-         RemainAfterExist = true;
+         RemainAfterExit = true;
       };
 
       script = ''
+         install -d -m 0755 -o rom -g users /home/rom
          rm -rf /home/rom/.dotfiles
-         mkdir -p /home/rom/.dotfiles
-         mkdir -p /home/rom/.config
-         cp -R ${inputs.dotfiles}/. /home/rom/.dotfiles/
+         install -d -m 0755 -o rom -g users /home/rom/.dotfiles /home/rom/.config
+         cp -R --no-preserve=mode,ownership ${inputs.dotfiles}/. /home/rom/.dotfiles/
 
          chown -R rom:users /home/rom/.dotfiles
          chown -R rom:users /home/rom/.config
+         chmod -R u+rwX /home/rom/.dotfiles /home/rom/.config
 
-         sudo -u rom HOME=/home/rom /home/rom/.dotfiles/link_all.bash
+         runuser -u rom -- env HOME=/home/rom bash -lc 'cd "$HOME" && bash "$HOME/.dotfiles/link_all.bash"'
       '';
    };
 
@@ -49,8 +99,6 @@ in
          };
       }
 
-      # hardware.pc
-
       "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
 
       unstable-hyprland
@@ -67,4 +115,3 @@ in
       networking
    ];
 }
-
